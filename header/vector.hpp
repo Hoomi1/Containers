0a7c0d998860e16169e3c1fa70fda28c18355851
Hoomi1 <glebbambam@yandex.ru>
2021-10-13 14:43:53 +0300

    vector::clear, reserve, =, copy constr

--- header/vector.hpp
+++ header/vector.hpp
@@ -13,28 +13,65 @@ namespace ft
 			size_t	m_size;
 			size_t	m_capacity;
 		public:
-			Vector() : m_arr(new T[0]), m_size(0), m_capacity(0) {};
+			Vector() : m_arr(new T[0]), m_size(0), m_capacity(0) {}
 
 			~Vector()
 			{
-				if (_size > 1)
+				if (m_size > 1)
 					delete[] m_arr;
 				delete m_arr;
 			}
 
-			Vector(const Vector<T> &other)
+			Vector(const Vector<T> &other) :
+			m_arr(new T[m_capasity]),
+			m_size(other.m_size),
+			m_capacity(other.m_capacity)
 			{
-				*this = other;
+				for (int i = 0; i < other.m_size; ++i)
+					this->m_arr[i] = other.m_arr[i];
 			}
 
-			Vector &operator = (const Vector<T> &other)/////
+			Vector &operator = (const Vector<T> &other)
 			{
+				if (other.m_size > this->m_size)
+				{
+					delete[] this->m_arr;
+					this->m_capacity = other.m_size * 2;
+					this->m_arr = new T[capacity];
+				}
+				for (int i = 0; i < other.m_size; ++i)
+					this->m_arr[i] = other.m_arr[i];
+				
+				this->m_size = other.m_size;
+
 				return (*this);
+			}
+
+			Vector(size_t new_size)//////////
+			{
+
+			}
+
+			Vector(size_t new_size, int index)///////////
+			{
+
+			}
+
+			void reserve(size_t new_cap)
+			{
+				T *new_arr = new T[new_cap];
+				for (int i = 0; i < this->m_size;++i)
+					new_arr[i] = this->m_arr[i];
+				this->capacity = new_cap;
+				delete[] m_arr;
+				this->m_arr = new_arr;
 			}
 
 			void resize(size_t n, const T &value = T())//////
 			{
-
+				if (n > this->capacity)
+					reserve(n);
+				
 			}
 
 			size_t size() const
@@ -52,6 +89,15 @@ namespace ft
 				if (m_size >= 1)
 					return (false);
 				return (true);
+			}
+
+			void clear()
+			{
+				// for (int i = 0; i < m_size; ++i)
+				// 	(reinterpret_cast<T*>(m_arr)[i]).~T();
+				this->m_size = 0;
+				this->m_capacity = 0;
+				this->m_arr = 0;
 			}
 	};
 }

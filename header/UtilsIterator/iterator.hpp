6df3bad23e9937b4551a9d9dd37aa644bf908a39
Hoomi1 <glebbambam@yandex.ru>
2021-10-27 21:14:37 +0300

    iterator: lexicogrf..., make_pair, enable_is

--- header/UtilsIterator/iterator.hpp
+++ header/UtilsIterator/iterator.hpp
@@ -1,5 +1,5 @@
-#ifndef UTILS_HPP
-#define UTILS_HPP
+#ifndef ITERATOR_HPP
+#define ITERATOR_HPP
 
 #include <iostream>
 //#include <iterator>
@@ -23,7 +23,7 @@ namespace ft
 	template<class T>
 	struct iterator_traits<T*>
 	{
-		typedef ptrdiff_t					difference_type;
+		typedef ptrdiff_t						difference_type;
 		typedef T								value_type;
 		typedef T*								pointer;
 		typedef T&								reference;
@@ -33,7 +33,7 @@ namespace ft
 	template<class T>
 	struct iterator_traits<const T*>
 	{
-		typedef ptrdiff_t					difference_type;
+		typedef ptrdiff_t						difference_type;
 		typedef T								value_type;
 		typedef const T*						pointer;
 		typedef const T&						reference;
@@ -48,8 +48,8 @@ namespace ft
 			typedef Iter													iterator_type;
 			typedef typename ft::iterator_traits<Iter>::iterator_category	iterator_category;
 			typedef typename ft::iterator_traits<Iter>::value_type			value_type;
-			typedef typename ft::iterator_traits<Iter>::difference_type	difference_type;
-			typedef typename ft::iterator_traits<Iter>::pointer			pointer;
+			typedef typename ft::iterator_traits<Iter>::difference_type		difference_type;
+			typedef typename ft::iterator_traits<Iter>::pointer				pointer;
 			typedef typename ft::iterator_traits<Iter>::reference			reference;
 		protected:
 			iterator_type	itr;
@@ -183,6 +183,19 @@ namespace ft
 	}
 	////////////////////////////////////////////////////////////
 	/////////////////////// *enable_if* ///////////////////////
+	template<bool B, class T = void>
+	struct enable_if
+	{
+		
+	};
+
+	template<class T>
+	struct enable_if<true, T>
+	{
+		typedef T type;
+	};
+	////////////////////////////////////////////////////////////
+	////////////////////////// *pair* //////////////////////////
 	template<class T1, class T2>
 	struct pair
 	{
@@ -225,10 +238,47 @@ namespace ft
 	};
 
 	template< class T1, class T2 >
-	std::pair<T1,T2> make_pair(T1 t, T2 u)
+	ft::pair<T1,T2> make_pair(T1 t, T2 u)
 	{
-
+		return (ft::pair<T1,T2>(t, u));
 	}
 	////////////////////////////////////////////////////////////
+	////////////////////// *is_integral* //////////////////////
+	template< class T >
+	struct is_integral
+	{
+		
+	};
+	///////////////////////////////////////////////////////////
+	//////////////// *lexicographical_compare* ////////////////
+	template<class InputIt1, class InputIt2>
+	bool lexicographical_compare(InputIt1 first1, InputIt1 last1,
+								InputIt2 first2, InputIt2 last2)
+	{
+		for (; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2)
+		{
+			if (*first1 < *first2)
+				return (true);
+			if (*first2 < *first1)
+				return (false);
+		}
+		return ((first1 == last1) && (first2 != last2));
+	}
+
+	template<class InputIt1, class InputIt2, class Compare>
+	bool lexicographical_compare(InputIt1 first1, InputIt1 last1,
+								InputIt2 first2, InputIt2 last2,
+								Compare comp)
+	{
+		for (; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2)
+		{
+			if (comp(*first1, *first2))
+				return (true);
+			if (comp(*first2, *first1))
+				return (false);
+		}
+		return (first1 == last1) && (first2 != last2);
+	}
+	///////////////////////////////////////////////////////////
 }
 #endif
